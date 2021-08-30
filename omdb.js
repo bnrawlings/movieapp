bc79b393c67dd7bb2981c7ccd8e6c280ffe8d07c
Britt Rawlings <88103399+bnrawlings@users.noreply.github.com>
2021-08-29 19:05:33 -0500

    OMDB Movie App CCS, JS, index.html files

---
+++ omdb.js
@@ -0,0 +1,30 @@
+// console.log ("I'm connected")
+
+const apiKey = "a8956cc"
+const omdbUrl = `http://www.omdbapi.com/?apikey=${apiKey}`
+
+
+const movieDeckcon = document.querySelector(".movieDeck")
+const searchButton = document.querySelector(".searchButton")
+
+async function getMovies(){
+    const input = document.querySelector(".inputField").value;
+    const movieData = await fetch(omdbUrl + `&s=${input}`);
+    const jsonMovies = await movieData.json();
+    // console.log(jsonMovies)
+    
+  for (const movie of jsonMovies.Search){
+    const movieDeck = document.createElement("div");
+    const movieTitle = document.createElement("h3");
+        const movieImg = document.createElement("img");
+      //  console.log(movie)
+        movieTitle.innerHTML = movie.Title;
+        movieImg.src = movie.Poster;
+        movieDeck.append(movieImg, movieTitle);
+        movieDeckcon.append(movieDeck);
+      }
+  }
+  
+
+
+searchButton.addEventListener("click", () => getMovies());
